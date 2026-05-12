11b6fe3328987b19a660a87248efd7f7d3fece1a
ks9019928-hub <ks9019928@gmail.com>
2026-05-13 00:19:50 +0530

    added resume upload AI

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,UploadFile,File
+import shutil
 
 app=FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Resume Screening API Running"}
+
+@app.post("/upload")
+async def upload_resume(file: UploadFile = File(...)):
+    file_location=f"uploads/{file.filename}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "filename": file.filename,
+        "message": "Resume uploaded successfully"
+    }
