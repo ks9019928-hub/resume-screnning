9cd591b2286b31b2d2086c41a57fa54e46d4d86a
ks9019928-hub <ks9019928@gmail.com>
2026-07-14 23:50:52 +0530

    updated dashboard

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,9 +1,36 @@
+import Navbar from "../components/layout/Navbar";
+import Sidebar from "../components/layout/Sidebar";
+
 function Dashboard() {
+
   return (
-    <div>
-      Dashboard
+
+    <div className="min-h-screen bg-gray-100">
+
+      <Navbar />
+
+      <div className="flex">
+
+        <Sidebar />
+
+        <main className="flex-1 p-10">
+
+          <h1 className="text-4xl font-bold mb-4">
+            Dashboard
+          </h1>
+
+          <p className="text-gray-600">
+            Welcome to HireMind AI
+          </p>
+
+        </main>
+
+      </div>
+
     </div>
+
   );
+
 }
 
 export default Dashboard;
