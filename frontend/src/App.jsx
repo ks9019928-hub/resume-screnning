550e3fe568d6e91cabe56110ea7319c47e5a09e1
ks9019928-hub <ks9019928@gmail.com>
2026-07-23 23:58:42 +0530

    updated app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 <BrowserRouter>
   <Routes>
     <Route path="/" element={<Login />} />
     <Route path="/login" element={<Login />} />
-    <Route path="/dashboard" element={<Dashboard />} />
+    <Route
+  path="/dashboard"
+  element={
+    <ProtectedRoute>
+      <Dashboard />
+    </ProtectedRoute>
+  }
+/>
   </Routes>
 </BrowserRouter>
