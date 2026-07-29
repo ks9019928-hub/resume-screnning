54daa828ad3bc7889307487d8b794c77792f9838
ks9019928-hub <ks9019928@gmail.com>
2026-07-29 23:47:46 +0530

    imported hero section to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import Navbar from "../components/layout/Navbar";
+import Navbar from "../components/layout/Navbar";import Navbar from "../components/layout/Navbar";
+import Hero from "../components/sections/Hero";
 
 export default function Home() {
   return (
-    <main className="bg-black text-white min-h-screen">
+    <main className="bg-black text-white">
+
       <Navbar />
+
+      <Hero />
+
     </main>
   );
 }
+
