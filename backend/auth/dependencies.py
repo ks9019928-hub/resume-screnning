058754b934fe04ab2efd62ac758d99e47b02b8cb
ks9019928-hub <ks9019928@gmail.com>
2026-07-30 02:53:29 +0530

    added more dependency

---
+++ backend/auth/dependencies.py
@@ -0,0 +1,21 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from auth.jwt_handler import verify_token
+
+security = HTTPBearer()
+
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+    token = credentials.credentials
+
+    try:
+        payload = verify_token(token)
+        return payload
+
+    except Exception:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token"
+        )
