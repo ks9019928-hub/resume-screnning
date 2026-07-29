e6d84911dcb0410c230f20478fe0988bbc22966e
ks9019928-hub <ks9019928@gmail.com>
2026-07-29 23:45:01 +0530

    home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/layout/Navbar";
+
+export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen">
+      <Navbar />
+    </main>
+  );
+}
