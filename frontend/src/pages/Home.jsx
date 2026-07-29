bbdaa21eb2d51e27f5f86daa407ad91e5d86214b
ks9019928-hub <ks9019928@gmail.com>
2026-07-29 23:49:46 +0530

    search into home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import Navbar from "../components/layout/Navbar";import Navbar from "../components/layout/Navbar";
 import Hero from "../components/sections/Hero";
+import Navbar from "../components/layout/Navbar";
+import Hero from "../components/sections/Hero";
+import SearchChanged from "../components/sections/SearchChanged";
 
 export default function Home() {
   return (
     <main className="bg-black text-white">
-
       <Navbar />
-
       <Hero />
-
+      <SearchChanged />
     </main>
   );
 }
