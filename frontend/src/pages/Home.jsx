18735698f9133e4da6ba3ed7b13aaa6a8ccc5ddf
ks9019928-hub <ks9019928@gmail.com>
2026-07-30 01:56:53 +0530

    updates

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,22 @@
-import Navbar from "../components/layout/Navbar";import Navbar from "../components/layout/Navbar";
+import Navbar from "../components/layout/Navbar";
 import Hero from "../components/sections/Hero";
 import Navbar from "../components/layout/Navbar";
 import Hero from "../components/sections/Hero";
 import SearchChanged from "../components/sections/SearchChanged";
+import Mission from "../components/sections/Mission";
 
 export default function Home() {
   return (
     <main className="bg-black text-white">
+
       <Navbar />
+
       <Hero />
+
       <SearchChanged />
+
+      <Mission />
+
     </main>
   );
 }
