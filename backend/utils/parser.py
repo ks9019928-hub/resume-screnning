469c4abfd9aa4214c8b1e19d134853fa05e3404e
ks9019928-hub <ks9019928@gmail.com>
2026-08-06 23:20:03 +0530

    updated prasing

--- backend/utils/parser.py
+++ backend/utils/parser.py
@@ -2,18 +2,19 @@ import pdfplumber
 from data.skills import SKILLS
 
 def extract_text(pdf_path):
-    text=""
+    text = ""
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
-            extracted=page.extract_text()
+            extracted = page.extract_text()
             if extracted:
-                text+=extracted+"\n"
+                text += extracted + "\n"
     return text
 
 def extract_skill(text):
-    found_skills=[]
-    text=text.lower()
+    found_skills = []
+    text = text.lower()
     for skill in SKILLS:
-        if skill.lower in text:
+        # CORRECTION: Added parentheses to properly call the lower() method
+        if skill.lower() in text:
             found_skills.append(skill)
     return found_skills
