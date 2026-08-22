01d1d2ba97a49ca7056f79f4ea2e2fd8f7a3fd78
ks9019928-hub <ks9019928@gmail.com>
2026-08-22 23:02:32 +0530

    fix(services): remove manual token header override in history service

--- frontend/src/services/history.js
+++ frontend/src/services/history.js
@@ -1,18 +1,6 @@
 import API from "./api";
 
 export const getResumeHistory = async () => {
-
-    const token = localStorage.getItem("token");
-
-    const response = await API.get(
-        "/resume/history",
-        {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }
-    );
-
-    return response.data;
-
+  const response = await API.get("/resume/history");
+  return response.data;
 };
